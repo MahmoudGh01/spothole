d9bd2615e4325de1d3f95aa3b2d6efe7863a5ad8
MahmoudGh01 <mahmoud.gharbi@esprit.tn>
2024-07-16 02:06:03 +0100

    flutter create .

--- android/app/src/main/java/com/example/job_seeker/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.job_seeker;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/com/example/boilerplate_flutter_login/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.boilerplate_flutter_login
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
